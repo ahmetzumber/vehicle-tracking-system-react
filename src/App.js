94d83e42786cc182d615cf024a04d05db73658da
Ahmet <ahmetzumber5@gmail.com>
2022-01-26 14:48:44 +0300

    project initialized

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from '../src/components/Navbar'
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
